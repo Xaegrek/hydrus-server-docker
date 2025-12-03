ddf976b87c756de1aa9be775a99404a2e9062400
Xaegrek <Xaegrek@gmail.com>
2025-12-02 23:08:48 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+# checkout master
 git checkout master
+# update submodules
 git submodule update --init --recursive
 git submodule foreach "git checkout tags/$1 || exit 1"
+# commit submodule updated HEAD
 git commit -a -m "$1"
+# tag commit
 git tag $1
+# push commit
 git push
-git push orgin $1
+#push tag
+git push master $1
